cbcd9842935b3fbcce5411df29e7efc97bdfab97
shilpa2603 <77782426+shilpa2603@users.noreply.github.com>
2023-08-18 19:25:02 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,22 +1,20 @@
-# Load libraries
-library(shiny)
-library(tidyverse)
+
 
 # Application Layout
 shinyUI(
   fluidPage(
     br(),
     # TASK 1: Application title
-    titlePanel("Final Project Phuong Linh"),
+    titlePanel(title ="Final Project Phuong Linh"),
     p("Explore the difference between people who earn less than 50K and more than 50K. You can filter the data by country, then explore various demogrphic information."),
     
     # TASK 2: Add first fluidRow to select input for country
     fluidRow(
       column(12, 
-             wellPanel("country", label = "Country", 
+             wellPanel(inputId ="country", label = "Country", 
                        selectInput("country_select", label = NULL, 
-                                   choices = c("United-States", "Canada", "Mexico", "Germany", "Philippines"))
-             )
+                                   choices = c("United-States", "Canada", "Mexico", "Germany", "Philippines"),
+                                  selected = "United-States"))
       )
     ),
     
@@ -25,12 +23,11 @@ shinyUI(
       column(3, 
              wellPanel(
                p("Select a continuous variable and graph type (histogram or boxplot) to view on the right."),
-               radioButtons("continuous_variable", label = "Continuous", choices = c("ages", "hours_per_week")),   # add radio buttons for continuous variables
-               radioButtons("graph_type", label = "Graph", choices = c("histogram", "boxplot"))    # add radio buttons for chart type
+               radioButtons(inputId ="continuous_variable", label = "Continuous", choices = c("ages", "hours_per_week"), selected = "histogram"),   # add radio buttons for continuous variables
+               radioButtons(inputId ="graph_type", label = "Graph", choices = c("histogram", "boxplot"), selected = "histogram")    # add radio buttons for chart type
                )
              ),
-      column(9, 
-             plotOutput("p1"))  # add plot output
+      column(9, plotOutput("p1"))  # add plot output
     ),
     
     # TASK 4: Add third fluidRow to control how to plot the categorical variables
@@ -39,7 +36,8 @@ shinyUI(
              wellPanel(
                p("Select a categorical variable to view bar chart on the right. Use the check box to view a stacked bar chart to combine the income levels into one graph. "),
                radioButtons("categorical_variable", label = "Categorical",
-                            choices = c("education", "workclass", "sex")),    # add radio buttons for categorical variables
+                            choices = c("education", "workclass", "sex"),
+                                selected = "education"),    # add radio buttons for categorical variables
                checkboxInput("is_stacked", label = "Stack bars", value = FALSE)     # add check box input for stacked bar chart option
                )
              ),
